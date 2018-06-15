4acffd2d6add52ccd9d2de27eeb209a37b07143f
yarden <yardench@gmail.com>
2018-06-15 04:31:54 -0700

    DONE encoding. Hope

--- ex5.pl
+++ ex5.pl
@@ -26,14 +26,18 @@ solveRadio(Instance,Solution) :-
 encodeRadio(graph(_, N, Edges), Map, MinCs, Cs):-
     findall(V, between(1,N,V), VertList), %get vartices list
     length(Map, N),
-    encode_strongly_connected_graph(N, Edges, Map, VertList, Cs - Cs1). %encode all the paths.
-    
+    encode_strongly_connected_graph(N, Edges, Map, VertList, Cs - Cs1), %encode all the paths.
+    write("After all the proccess, Map is:"),
+    print(Map),
+    print("CS is:"),
+    print(Cs),
+    sum_radios(N, Map, MinCs, Cs1- []).
+
 %encode_strongly_connected_graph(+, +, -, +, Cs - Cs1). 
 %verify that:
 %1. the grpah is strongly connected
 %2. check that there is a path from V1 to V2 and vise vera, for each V2-VN in th e graph.
 %that will be enough to ensure that from any V there is a path to any other V.
-
 encode_strongly_connected_graph(N, Edges, Map, [V1 |VertList], Cs - Cs1):-
     encode_strongly_connected_graph_helper(N, Edges, Map, V1, VertList, Cs - Cs1).
 
@@ -42,4 +46,74 @@ encode_strongly_connected_graph_helper(_, _, _, _, [], Cs - Cs).
 encode_strongly_connected_graph_helper(N, Edges, Map, V1, [V2|VertList], Cs - Cs1):-
     encode_path_from_v1_to_v2(N, Edges, Map, V1, V2, Cs - CsM),
     encode_path_from_v1_to_v2(N, Edges, Map, V2, V1, CsM - CsM1),
-    encode_strongly_connected_graph_helper(N, Edges, Map, V1, VertList, CsM - Cs1).
+    encode_strongly_connected_graph_helper(N, Edges, Map, V1, VertList, CsM1 - Cs1).
+
+%encode_path_from_v1_to_v2(+,+,-,+,+,-)
+encode_path_from_v1_to_v2(N, Edges, Map, V1, Vn, Cs1 - Cs3):-
+    length(Path, N),
+    nth1(1, Path, V1),
+    nth1(N, Path, Vn),
+    declare_nums(Path, 1, N, Cs1-Cs2),
+    encode_valid_path(N, Edges, Map, Path, Cs2 - Cs3). 
+
+%declare_nums(-, +, +, -),
+declare_nums([], _,_,Cs-Cs).
+declare_nums([Num|Nums], Min, Max, [new_int(Num, Min, Max)|Cs1]- Cs2):-
+    var(Num),
+    !,
+    declare_nums(Nums, Min, Max, Cs1-Cs2),
+    !.
+declare_nums([_|Nums], Min, Max, Cs):-
+    declare_nums(Nums, Min, Max, Cs).
+
+%Encode valid path from V1 to V2
+%encode_valid_path(+, +, -, +, -).
+%Even path base case
+encode_valid_path(_, _, _, [], Cs-Cs).
+%Odd path base case
+encode_valid_path(_, _, _, [_], Cs-Cs).
+encode_valid_path(N, Edges, Map, [V1, V2|Path], Cs1 - Cs3):-
+    encode_valid_edge(Edges, Map, V1, V2, Cs1-Cs2),
+    encode_valid_path(N, Edges, Map, [V2|Path], Cs2-Cs3).
+   
+
+%Encode that each neighbor pair are valid.
+%1st case --> Self loop.
+encode_valid_edge(Edges, Map, V1, V2, [int_eq_reif(V1, V2, X),
+                                    bool_array_or([X|Xs])| Cs1] - Cs2):-
+    encode_valid_edge_case_two(Edges, Map, V1, V2, Xs, Cs1-Cs2).
+
+%base case of /6
+encode_valid_edge_case_two([], _, _ , _, [], Cs-Cs).
+%2nd case --> Edge weight 1.
+encode_valid_edge_case_two([(A1, A2,1)|RestEdges],Map, V1, V2, [X,XX|XS], 
+                                                    [%in this case V1 is A1
+                                                     %V2 is A2
+                                                     int_eq_reif(V1, A1, X1),
+                                                     int_eq_reif(V2, A2, X2),
+                                                     bool_and_reif(X1, X2, X),
+                                                     %opposite
+                                                     int_eq_reif(V1, A2, XX1),
+                                                     int_eq_reif(V2, A1, XX2),
+                                                     bool_and_reif(XX1, XX2, XX)
+                                                     |Cs1] - Cs2):-
+    encode_valid_edge_case_two(RestEdges, Map, V1, V2, XS, Cs1-Cs2).
+%2nd case --> Edge weight 2.
+encode_valid_edge_case_two([(A1, A2,2)|RestEdges],Map, V1, V2, [X,XX|XS],
+                                                    [%in this case V1 is A1
+                                                     %V2 is A2
+                                                     int_eq_reif(V1, A1, X1),
+                                                     int_eq_reif(V2, A2, X2),
+                                                     bool_array_and_reif([X1, X2, B1],X),
+                                                     %opposite
+                                                     int_eq_reif(V1, A2, XX1),
+                                                     int_eq_reif(V2, A1, XX2),
+                                                     bool_array_and_reif([XX1, XX2, B2],XX)
+                                                    |Cs1] -Cs2):-
+    nth1(A1, Map, B1),
+    nth1(A2, Map, B2),
+    encode_valid_edge_case_two(RestEdges, Map, V1, V2, XS, Cs1-Cs2).
+
+%sun the number of STRONG transmiters.
+sum_radios(N, Map, Sum, [new_int(Sum, 0, N),
+                        bool_array_sum_eq(Map, Sum)| Cs] - Cs).
--- pg.pl
+++ pg.pl
@@ -5,3 +5,7 @@ countTwos([(_,1)|RestSol], Counter):-
 countTwos([(_,2)|RestSol], Counter):-
     countTwos(RestSol, Counter1),
     Counter is Counter1+1.
+
+
+graph('g1', 4, [(1,2,2), (2,3,1), (3,4,1), (4,2,1), (1,3,2)])
+
